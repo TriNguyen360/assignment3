9a3f6a14317bc254eb9c488a4012539de443f5f0
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-10-31 18:05:48 -0400

    test 2

--- apps/shopping/models.py
+++ apps/shopping/models.py
@@ -1,21 +1,27 @@
 """
-This file defines the database models
+This file defines the database models.
 """
 
 import datetime
-from .common import db, Field, auth
-from pydal.validators import *
+from pydal.validators import IS_NOT_EMPTY
+from .common import db, Field, auth  # Import necessary objects from common.py
 
+# Define utility functions
+def get_time():
+    return datetime.datetime.utcnow()
 
 def get_user_email():
     return auth.current_user.get('email') if auth.current_user else None
 
-def get_time():
-    return datetime.datetime.utcnow()
+# Define the database table for the shopping list
+db.define_table(
+    'shopping_list',
+    Field('item_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('is_purchased', 'boolean', default=False),
+    Field('user_email', 'string', default=get_user_email),
+    Field('added_on', 'datetime', default=get_time),
+    Field('checked_on', 'datetime', default=None),
+)
 
-# Add here any table definition you need. Below is an example.
-# db.define_table('shopping_list',
-#    Field('product_name', requires=IS_NOT_EMPTY()),
-#    )
-
+# Commit the changes to the database
 db.commit()
